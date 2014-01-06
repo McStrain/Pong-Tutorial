cd40662e2db9b05fc865c07abc8d68f75186b9ec
craig.brandstetter <cbrandst@gmail.com>
2014-01-06 16:25:05 -0600

    Pong after 3rd video

---
+++ Assets/Ball.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+// var cSpeed:float = 10.0;
+// var sFactor:float = 10.0;
+
+function Start () 
+{
+	rigidbody2D.velocity.y = 5;
+	rigidbody2D.velocity.x = 5 * -1;
+
+}
+
+function Update () 
+{
+
+}
